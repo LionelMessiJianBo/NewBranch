b164a3d39f0c6a50be06f943219762f44fec1a74
bjb <1907386312@qq.com>
2017-08-15 10:00:42 +0800

    提交

---
+++ TEXT1/TEXT1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEXT1
+//
+//  Created by pdid on 2017/8/15.
+//  Copyright © 2017年 pdid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEXT1/TEXT1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEXT1
+//
+//  Created by pdid on 2017/8/15.
+//  Copyright © 2017年 pdid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
